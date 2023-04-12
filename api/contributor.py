4ac3a2385234db8a30fee31c19727a72c8c500f1
fan-droide <jose.gilpanal@telecom-paris.fr>
2023-04-12 12:30:55 +0200

    add contributor by id front and back end

--- api/contributor.py
+++ api/contributor.py
@@ -2,7 +2,7 @@ import os
 import re
 from flask import request, jsonify
 
-def addcontributor(current_user, Composition, Contributor, User, db):
+def addcontributorbyemail(current_user, Composition, Contributor, User, db):
   
     user_auth = current_user.get_id()
     compositionid = request.get_json()['composition_id']
@@ -23,23 +23,51 @@ def addcontributor(current_user, Composition, Contributor, User, db):
         
         # if Owner tries to add himself throws error
         if((user2 is not None) and (current_user.email != email) and (match is not None) and (1 <=role <=4)):            
-            querycontributor = Contributor.query.filter_by(user_id=user2.id, composition_id=compositionid)
-            iscontributor = querycontributor.first()                    
-            # if is already contributor => UPDATE role
-            if(iscontributor is not None):
-                querycontributor.update({"role":role})
-                db.session.commit()
-                return jsonify({"ok":"true", "result":"role updated successfully"})
-            else:    
-                contributor = Contributor(role=role, user_id=user2.id, composition=composition)          
-                db.session.add(contributor)
-                db.session.commit()        
-                return jsonify({"ok":"true", "result":"role added successfully"})
+            return addcontributortodb(Contributor, user2.id, composition, role, db)
         else:
             return jsonify({"error":"not valid contributor"})
     else:
         return jsonify({"error":"not valid owner"})
 
+
+def addcontributorbyid(current_user, Composition, Contributor, User, db):
+  
+    user_auth = current_user.get_id()
+    compositionid = request.get_json()['composition_id']
+    composition = Composition.query.get_or_404(compositionid)
+    
+    # if the person who tries to add the contributor is the owner
+    # TODO: check if the role is 1
+    # control that permission is not changed to creator of the composition
+    if composition.user.id == user_auth:        
+        userid = request.get_json()["user_id"] 
+                       
+        role = request.get_json()['role']  
+        # check the ID is in DB      
+        user2 = User.query.filter_by(id=userid).first()
+               
+        # if Owner tries to add himself throws error
+        if((user2 is not None) and (user_auth != userid) and (1 <=role <=4)):            
+            return addcontributortodb(Contributor, user2.id, composition, role, db)
+        else:
+            return jsonify({"error":"not valid contributor"})
+    else:
+        return jsonify({"error":"not valid owner"})
+
+def addcontributortodb(Contributor, user2id, composition, role, db):
+    querycontributor = Contributor.query.filter_by(user_id=user2id, composition_id=composition.id)
+    iscontributor = querycontributor.first()                    
+    # if is already contributor => UPDATE role
+    if(iscontributor is not None):
+        querycontributor.update({"role":role})
+        db.session.commit()
+        return jsonify({"ok":"true", "result":"role updated successfully"})
+    else:    
+        contributor = Contributor(role=role, user_id=user2id, composition=composition)          
+        db.session.add(contributor)
+        db.session.commit()        
+        return jsonify({"ok":"true", "result":"role added successfully"})
+    
 def deletecontributor(contribid, current_user, Composition, Contributor, db):
     user_auth = current_user.get_id()
     contributor = Contributor.query.get(contribid)
