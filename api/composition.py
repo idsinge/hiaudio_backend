4c9d47c5f3a25e670cf2b6266e638857900464b3
fan-droide <jose.gilpanal@telecom-paris.fr>
2023-06-09 16:25:00 +0200

    privacy as enum

--- api/composition.py
+++ api/composition.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from flask import request, jsonify
 from api.track import DATA_BASEDIR
-from orm import UserRole
+from orm import UserRole, CompPrivacy
 
 
 def compositions(current_user, Composition, Contributor):
@@ -10,10 +10,10 @@ def compositions(current_user, Composition, Contributor):
     allcompositions = Composition.query.all()
     compositions = []
     for comp in allcompositions:
-        if ((user_auth is None) and (comp.privacy == 1)):           
+        if ((user_auth is None) and (comp.privacy.value == CompPrivacy.public.value)):           
             compositions.append(comp)
         else:
-            if((comp.privacy != 3 ) and (user_auth is not None)):                
+            if((comp.privacy.value != CompPrivacy.private.value ) and (user_auth is not None)):                
                 compositions.append(comp)
             else:
                 if(comp.user.id == user_auth):
@@ -27,13 +27,13 @@ def compositions(current_user, Composition, Contributor):
     return jcompositions
 
 
-# if privacy= 2 or 3, and not logged => not accesible
-# if privacy=3 and not either owner/contributor => not accesible
+# if privacy= 2 (onlyreg) or 3 (private), and not logged => not accesible
+# if privacy=3 (private) and not either owner/contributor => not accesible
 
 def composition(id, current_user, Composition, Contributor):
     user_auth = current_user.get_id() and int(current_user.get_id())
     composition = Composition.query.get_or_404(id)
-    if ((user_auth is None) and ((composition.privacy == 2) or (composition.privacy == 3))):
+    if ((user_auth is None) and ((composition.privacy.value == CompPrivacy.onlyreg.value) or (composition.privacy.value == CompPrivacy.private.value))):
         return jsonify({"error":"composition not accesible"})
     else:
         owner = composition.user.id == user_auth
@@ -49,7 +49,7 @@ def composition(id, current_user, Composition, Contributor):
               role = UserRole.owner.value          
         if(iscontributor is not None):
             role = iscontributor.role.value            
-        if((composition.privacy == 3) and (composition.user.id != user_auth) and (role == UserRole.none.value)):
+        if((composition.privacy.value == CompPrivacy.private.value) and (composition.user.id != user_auth) and (role == UserRole.none.value)):
             return jsonify({"error":"composition not accesible"})
         else:
             data = composition.to_dict( rules=('-path',) )
@@ -63,9 +63,9 @@ def newcomposition(current_user,User, Composition, db):
     if current_user.is_authenticated:
         title = request.get_json()["title"]
         privacy = request.get_json()["privacy_level"]
-        if(privacy and (privacy is not None) and (1<=int(privacy) <=3)):
+        if(privacy and (privacy is not None) and (CompPrivacy.public.value <= int(privacy) <= CompPrivacy.private.value)):
             user = User.query.get(current_user.get_id())
-            composition = Composition(title=title, user=user, privacy=privacy)
+            composition = Composition(title=title, user=user, privacy=CompPrivacy(int(privacy)).name)
             db.session.add(composition)
             db.session.commit()
             return jsonify(composition=composition.to_dict( rules=('-path',) ))
@@ -114,7 +114,9 @@ def updatecomptocontrib(current_user, Composition, Contributor, db):
 
 def updatecompfield(current_user, Composition, Contributor, db, field):
     compid = request.get_json()['id']
-    fieldvalue = request.get_json()[field]    
+    fieldvalue = request.get_json()[field]
+    if(field == 'privacy'):        
+        fieldvalue = CompPrivacy(int(fieldvalue)).name
     user_auth = current_user.get_id() and int(current_user.get_id())
     composition =  Composition.query.get_or_404(compid)
     iscontributor = Contributor.query.filter_by(composition_id=composition.id, user_id=user_auth).first()       
