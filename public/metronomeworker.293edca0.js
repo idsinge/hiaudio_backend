718833db7cd826940819df493d088c632da6d475
fan-droide <jose.gilpanal@telecom-paris.fr>
2024-12-02 10:02:19 +0100

    issue-53: activate metronome menu bar after issue 194 toast track not saved

--- public/metronomeworker.293edca0.js
+++
@@ -1,2 +0,0 @@
-let t,a;t=null,a=100,self.onmessage=function(e){"start"==e.data?t=setInterval(function(){postMessage("tick")},a):e.data.interval?(a=e.data.interval,t&&(clearInterval(t),t=setInterval(function(){postMessage("tick")},a))):"stop"==e.data&&(clearInterval(t),t=null)};
-//# sourceMappingURL=metronomeworker.293edca0.js.map
