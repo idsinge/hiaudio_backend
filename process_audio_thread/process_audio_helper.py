60adc26c05d847dea14658fba9a549471b568fba
fan-droide <jose.gilpanal@telecom-paris.fr>
2025-05-06 17:46:39 +0200

    include instrument recognition and changes in  metadata fields

--- process_audio_thread/process_audio_helper.py
+++ process_audio_thread/process_audio_helper.py
@@ -14,33 +14,49 @@ from orm import db, Track
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import config
 from api.annotation import handle_new_track_annotation
+from instrument_recognition import make_instrument_pred
+from instrument_filtered_labels import percussion_instruments
+from annotation_utils import convertmetadata
 
 def process_audio_file(track, item):    
     try:
         fullpath = os.path.join(config.DATA_BASEDIR, track.path)
         metadata = mediainfo(fullpath)
         metadata.pop("filename", None)
-        setattr(track, 'file_metadata', metadata)
-        db.session.commit()
+        converted_metadata = convertmetadata(metadata)
         is_silence, rms_db = tellifsilence(fullpath)
-        handlegenerictrackannotation('is_silence', str(is_silence).lower(), track.uuid)
-        handlegenerictrackannotation('RMS', '{0:.2f}'.format(rms_db), track.uuid)
+        metadata["RMS"] ='{0:.2f} dB'.format(rms_db)
+        if is_silence:
+            handlegenerictrackannotation('is_silence', str(is_silence).lower(), track.uuid)      
+        
         if not is_silence:        
             is_speech_pred = tellifisspeech(fullpath)
-            handlegenerictrackannotation('is_speech_score', '{0:.2f}'.format(is_speech_pred), track.uuid)
+            handlegenerictrackannotation('is_speech_score', '{0:.2f}%'.format(is_speech_pred), track.uuid)
             if is_speech_pred < 50:
-                iscopyrightedannotation(fullpath, track.uuid)
+                is_copyrighted, fingerprint = iscopyrightedannotation(fullpath, track.uuid)
+                if is_copyrighted:
+                    metadata["fingerprint"] = fingerprint
                 tempoannotation(fullpath, track.uuid)
                 is_percussion = checkifpercurssion(fullpath)
                 handlegenerictrackannotation('is_percurssion', str(is_percussion).lower(), track.uuid)            
                 if not is_percussion:
                     tonalkeyscaleannotation(fullpath, track.uuid)
+                if 1.8 <= is_speech_pred < 50:
+                    handlegenerictrackannotation('instrument', 'voice', track.uuid)
+                else:
+                    instrumentannotation(fullpath, track.uuid, is_percussion)
+                  
+            else:
+                handlegenerictrackannotation('instrument', 'speech', track.uuid)
 
+        setattr(track, 'file_metadata', converted_metadata)
+        db.session.commit()
         set_track_as_processed(track)
 
     except Exception as e:
         set_track_as_processed(item)
         logging.info(f"{e}, " + f"{fullpath}")
+
 
 def handlegenerictrackannotation(key_annot, value_annot, track_uuid):
     annot_json = {'key':key_annot, 'value':value_annot}
@@ -52,19 +68,29 @@ def tempoannotation(fullpath, track_uuid):
     handlegenerictrackannotation('BPM', bpm_tempo, track_uuid)    
 
 def iscopyrightedannotation(fullpath, track_uuid):
-    is_copyrighted = checkifcopyright(fullpath)
+    is_copyrighted, fingerprint = checkifcopyright(fullpath)
     if is_copyrighted and len(is_copyrighted['results']) > 0:
-        handlegenerictrackannotation('is_copyrighted_score', '{0:.2f}'.format(is_copyrighted['results'][0]['score']*100), track_uuid)
+        handlegenerictrackannotation('is_copyrighted_score', '{0:.2f}%'.format(is_copyrighted['results'][0]['score']*100), track_uuid)
         recordings = is_copyrighted['results'][0].get('recordings')
         if recordings:
             artist_title =  get_first_artist_and_title(is_copyrighted)
             handlegenerictrackannotation('is_copyrighted_artist', artist_title.get('artist_name'), track_uuid)
             handlegenerictrackannotation('is_copyrighted_title', artist_title.get('title'), track_uuid)
+    return is_copyrighted, fingerprint
 
 def tonalkeyscaleannotation(fullpath, track_uuid):
     key, scale = getkeyandscale(fullpath)
     handlegenerictrackannotation('tonality_key', key, track_uuid)
     handlegenerictrackannotation('tonality_scale', scale, track_uuid)
+
+def instrumentannotation(fullpath, track_uuid, is_percussion):
+    instrum_label, instrum_score = make_instrument_pred(fullpath)
+    if is_percussion and (instrum_label not in percussion_instruments):
+        instrum_label = ''
+        instrum_score = 0
+    handlegenerictrackannotation('instrument', str(instrum_label).lower(), track_uuid)
+    handlegenerictrackannotation('instrument_score', '{0:.2f}%'.format(instrum_score*100), track_uuid)
+    
 
 def set_track_as_processed(item):
     try:
